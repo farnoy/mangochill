79c8e296096f7bfc5fe0e95f84ad61cbae2c5141
Jakub Okoński <jakub@okonski.org>
2026-04-16 11:36:10 +0200

    fix: try AllowRefreshSwitching again
    It seems to work now that we have reliable detection of gamescope's
    refresh rate feedback, and our integer divisor rounding works well.

--- src/gamescope.rs
+++ src/gamescope.rs
@@ -130,7 +130,7 @@ impl GamescopeConnection {
     pub fn set_fps(&mut self, fps: u16) -> anyhow::Result<()> {
         let _ = self.queue.dispatch_pending(&mut self.state);
 
-        let mut flags = gamescope_control::TargetRefreshCycleFlag::empty();
+        let mut flags = gamescope_control::TargetRefreshCycleFlag::AllowRefreshSwitching;
         if self.state.is_internal_display {
             flags |= gamescope_control::TargetRefreshCycleFlag::InternalDisplay;
         }
